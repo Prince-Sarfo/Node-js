37fcc088b821d4d8c74d88f08de83256d5552895
Prince-Sarfo <snellmaxi6@gmail.com>
2023-05-25 12:48:34 +0000

    initial commit of express

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
-const {v4 : uuid4}  = require('uuid');
-console.log(uuid4());
+// const {v4 : uuid4}  = require('uuid');
+// console.log(uuid4());
+
+// express
+const express = require('express');
+app = express();
+const PORT = 3000;
+
+app.get('/',(req, res) =>{
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT);
+});
+
+
