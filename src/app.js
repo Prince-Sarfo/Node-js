d927877cdfba955bb2d8641b05f3c8721b77b642
Prince-Sarfo <snellmaxi6@gmail.com>
2023-05-25 14:14:20 +0000

    using postman nodemon

--- src/app.js
+++ src/app.js
@@ -4,10 +4,47 @@
 // express
 const express = require('express');
 app = express();
+app.use(express.json());
+app.use(express.urlencoded({'extended':true}))
 const PORT = 3000;
+
+const Customers =[
+    {
+        "name": "Prince THE KARMA",
+        "industry" : "Tech"
+    },
+    {
+        "name": "Morrison",
+        "industry":"Music"
+    },
+    {
+        "name":"Freda",
+        "industry": "Government"
+    }
+]
+
+
+
+
+
+
 
 app.get('/',(req, res) =>{
     res.send("Hello World");
+});
+
+app.get('/api/customers', (req, res)=>{
+    res.send({"Customers" : Customers});
+});
+
+
+app.post("/api/customers",(req, res)=>{
+        res.send(req.body);
+        console.log(req.body);
+});
+
+app.post('/', (req, res)=>{
+    res.send("This is a post request");
 });
 
 app.listen(PORT, () => {
